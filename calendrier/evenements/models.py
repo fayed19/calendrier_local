1385940c8d07fb2083a64fcf64777970d63e8525
AYENA Eudes Fourier <ayena2fourier@gmail.com>
2025-05-09 19:07:32 +0100

    Création de l'application evenements avec le modèle Event

---
+++ calendrier/evenements/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Events(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
